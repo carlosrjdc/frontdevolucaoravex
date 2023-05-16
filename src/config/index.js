053cefb937820447504d179c359195185cf08dad
carlosrjdc <uppaplicativo@gmail.com>
2023-05-16 15:10:11 -0300

    Update index.js

--- src/config/index.js
+++ src/config/index.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const token = sessionStorage.getItem("tkn");
 
 const Axios = axios.create({
-  baseURL: "http://brlpvudh4vxb92.br.lactadom.ad:4000/",
+  baseURL: "http://brlpvudh4vxb92.br.lactadom.ad:4000",
   timeout: 5000,
   headers: { "X-Custom-Header": "foobar" },
 });
